342f0232dd3c60d73b58f968f737c52969ac6c05
Juan Chaur <juan.chaur89@gmail.com>
2015-09-12 23:42:22 +0200

    JC encapsuling Person Class

--- tdd-starter-kit/app/src/Person.js
+++ tdd-starter-kit/app/src/Person.js
@@ -1,14 +1,21 @@
+(function(window, document, undefined) {
+	'use strict';
 
+	function Person(name, age) {
+		this._name = name;
+		this._age = age;
+	}
 
-function Person(name, age) {
-	this._name = name;
-	this._age = age;
-}
+	Person.prototype.getName = function() {
+		return this._name;
+	};
 
-Person.prototype.getName = function() {
-	return this._name;
-};
+	Person.prototype.getAge = function() {
+		return this._age;
+	};
 
-Person.prototype.getAge = function() {
-	return this._age;
-};
+	//To export it for testing
+	window.Person = Person;
+
+}(window, document));
+
